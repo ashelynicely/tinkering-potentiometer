c610cc4c5306158e9324f26ddd2420796b8f32dd
ashelynicely <ashely.nicely@gmail.com>
2016-02-02 10:19:33 -0600

    added speaker

--- tinkering_potentiometer/tinkering_potentiometer.ino
+++ tinkering_potentiometer/tinkering_potentiometer.ino
@@ -10,10 +10,12 @@
 
 // The potentiometer is on port A2
 #define   POTENTIOMETER   A2
+#define   SPEAKER         5
 
 void setup() {
   // Sets the potentiometer port to "INPUT"
   pinMode(POTENTIOMETER, INPUT);
+  pinMode(SPEAKER, OUTPUT);
 
   // Starts communication with the computer
   Serial.begin(9600);
@@ -23,9 +25,13 @@ void loop() {
   // saves the reading of the potentiometer into "value
   int value = analogRead(POTENTIOMETER);
 
+  int freq = map(value, 0, 50, 50, 1); 
+
   // Prints the current reading
   Serial.println(value);
 
+  tone(SPEAKER, 3000);
   // waits
   delay(100);
+  
 }
